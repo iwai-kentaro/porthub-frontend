e5e3ef940182206e055bcce27d4de7ec1e449b7e
kentaro iwai <iwai.kentaro@wiserock.jp>
2025-02-18 07:42:23 +0900

    feat: getMeを作成

--- src/app/api/auth/getMe.ts
+++ src/app/api/auth/getMe.ts
@@ -2,8 +2,13 @@ import axiosClient from "@/app/lib/axiosClient";
 import User from "@/app/model/user";
 
 const getMe = async() => {
-    const res = await axiosClient.get('/auth/me' );
 
+    const token = localStorage.getItem('JWT');
+    const res = await axiosClient.get('/auth/me',{
+        headers: {
+            Authorization: `Bearer ${token}`
+        },
+    });
 
     const userJson: User[] = res.data.user ? res.data.user : res.data;
     return userJson.map((us) => User.buildFromJson(us));
